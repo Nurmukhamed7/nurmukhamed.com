982a6392f832eb210ee8ec124c2eacc542f0d898
Nurmukhamed <nurik.berikbolov@gmail.com>
2025-03-05 16:58:38 +0100

    feat: update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
     ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        rules: {
+            'react/no-unescaped-entities': [
+                'error',
+                {
+                    forbid: ['>', '}', '"'], // Allow single quotes (`'`) without escaping
+                },
+            ],
+        },
+    },
 ]
 
 export default eslintConfig
